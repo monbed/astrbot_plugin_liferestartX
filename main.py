9a0721f88340d66fe2c906e58471d2b65dfa67ba
monbed <67903399+monbed@users.noreply.github.com>
2025-05-29 05:47:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class LifeRestartPlugin(Star):
                     ps.append(tmp)
                 else:
                     if tmp >= 10:
-                        ps.append(random.randint(0, 50))
+                        ps.append(random.randint(0, 30))
                     else:
                         ps.append(random.randint(0, tmp))
                 tmp -= ps[-1]
